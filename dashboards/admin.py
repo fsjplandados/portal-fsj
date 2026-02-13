d9c134d7a78ee32144ee03320890f6a042c68e5c
fsjplandados <fsjplan.dados@gmail.com>
2026-02-13 16:18:29 -0300

    Update admin.py

--- dashboards/admin.py
+++ dashboards/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Categoria, Dashboard
+from .models import Categoria, Dashboard, LogAcesso
 
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
@@ -10,3 +10,10 @@ class DashboardAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'categoria', 'data_atualizacao', 'admin_only', 'is_new')
     list_filter = ('categoria', 'admin_only')
     search_fields = ('titulo', 'descricao')
+
+@admin.register(LogAcesso)
+class LogAcessoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'dashboard_titulo', 'data_acesso')
+    list_filter = ('usuario', 'data_acesso')
+    readonly_fields = ('usuario', 'dashboard_titulo', 'data_acesso') # Ninguém pode alterar o log
+    search_fields = ('usuario__username', 'dashboard_titulo')
